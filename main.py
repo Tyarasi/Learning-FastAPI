ff5cbb8f9c05799b766703a04f1c7e81beeb60f7
Teguh <teguhamanah8@gmail.com>
2023-12-18 15:49:52 +0700

    Belajar Routing FastAPI

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from typing import Union
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from auth_routes import auth_router
+from order_routes import order_router
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-    @app.get("/items/{item_id}")
-    def read_item(item_id: int, q: Unio n[str, None] = None):
-        return {"item_id": item_id, "q": q}
+app.include_router(auth_router)
+app.include_router(order_router)
